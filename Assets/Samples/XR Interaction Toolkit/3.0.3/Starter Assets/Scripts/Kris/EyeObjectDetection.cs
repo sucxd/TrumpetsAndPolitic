9f375914edb5ff0e899fe142d51da95ba692076d
sucxd <114672524+sucxd@users.noreply.github.com>
2024-10-08 15:20:03 +0100

    Fixed Compiler error

--- Assets/Samples/XR Interaction Toolkit/3.0.3/Starter Assets/Scripts/Kris/EyeObjectDetection.cs
+++ Assets/Samples/XR Interaction Toolkit/3.0.3/Starter Assets/Scripts/Kris/EyeObjectDetection.cs
@@ -1,54 +1,54 @@
-using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
+// using UnityEngine;
+// using UnityEngine.Rendering;
+// using UnityEngine.Rendering.Universal;
 
-public class EyeObjectDetection : MonoBehaviour
-{
-    public Camera mainCamera;
-    public float detectionDistance = 10f;
-    public string targetTag = "Interactable";
-    public Volume postProcessVolume;
-    private Vignette vignette;
+// public class EyeObjectDetection : MonoBehaviour
+// {
+//     public Camera mainCamera;
+//     public float detectionDistance = 10f;
+//     public string targetTag = "Interactable";
+//     public Volume postProcessVolume;
+//     private Vignette vignette;
 
-    private void Start()
-    {
-        postProcessVolume.profile.TryGet(out vignette);
-    }
+//     private void Start()
+//     {
+//         postProcessVolume.profile.TryGet(out vignette);
+//     }
 
-    private void Update()
-    {
-        bool objectDetected = DetectObject(mainCamera);
+//     private void Update()
+//     {
+//         bool objectDetected = DetectObject(mainCamera);
 
-        if (objectDetected)
-        {
-            ApplyVignette(0.5f);
-        }
-        else
-        {
-            ApplyVignette(0f);
-        }
-    }
+//         if (objectDetected)
+//         {
+//             ApplyVignette(0.5f);
+//         }
+//         else
+//         {
+//             ApplyVignette(0f);
+//         }
+//     }
 
-    private bool DetectObject(Camera eyeCamera)
-    {
-        Ray ray = new Ray(eyeCamera.transform.position, eyeCamera.transform.forward);
-        RaycastHit hit;
+//     private bool DetectObject(Camera eyeCamera)
+//     {
+//         Ray ray = new Ray(eyeCamera.transform.position, eyeCamera.transform.forward);
+//         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, detectionDistance))
-        {
-            if (hit.collider.CompareTag(targetTag))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+//         if (Physics.Raycast(ray, out hit, detectionDistance))
+//         {
+//             if (hit.collider.CompareTag(targetTag))
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
 
-    private void ApplyVignette(float intensity)
-    {
-        if (vignette != null)
-        {
-            vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, intensity, Time.deltaTime * 5f);
-        }
-    }
-}
+//     private void ApplyVignette(float intensity)
+//     {
+//         if (vignette != null)
+//         {
+//             vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, intensity, Time.deltaTime * 5f);
+//         }
+//     }
+// }
