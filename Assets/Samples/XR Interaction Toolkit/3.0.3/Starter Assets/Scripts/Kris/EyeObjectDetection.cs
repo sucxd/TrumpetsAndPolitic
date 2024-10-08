9ff11bd2dac36f442e0045d3195d196f0f9991d5
sucxd <114672524+sucxd@users.noreply.github.com>
2024-10-08 13:51:33 +0100

    Working on EyeObjectDetection

---
+++ Assets/Samples/XR Interaction Toolkit/3.0.3/Starter Assets/Scripts/Kris/EyeObjectDetection.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class EyeObjectDetection : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float detectionDistance = 10f;
+    public string targetTag = "Interactable";
+    public Volume postProcessVolume;
+    private Vignette vignette;
+
+    private void Start()
+    {
+        postProcessVolume.profile.TryGet(out vignette);
+    }
+
+    private void Update()
+    {
+        bool objectDetected = DetectObject(mainCamera);
+
+        if (objectDetected)
+        {
+            ApplyVignette(0.5f);
+        }
+        else
+        {
+            ApplyVignette(0f);
+        }
+    }
+
+    private bool DetectObject(Camera eyeCamera)
+    {
+        Ray ray = new Ray(eyeCamera.transform.position, eyeCamera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, detectionDistance))
+        {
+            if (hit.collider.CompareTag(targetTag))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void ApplyVignette(float intensity)
+    {
+        if (vignette != null)
+        {
+            vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, intensity, Time.deltaTime * 5f);
+        }
+    }
+}
